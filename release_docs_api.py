be36e71bfad9a5c4cb4a854199a1e4ad31e0c0cb
David Giffin <david@giffin.org>
2024-06-12 17:51:23 -0400

    Updating prompt

--- release_docs_api.py
+++ release_docs_api.py
@@ -70,20 +70,17 @@ llm_chain = configure_llm_only_chain(llm)
 # PROMPT TEMPLATE
 general_system_template = """
 Use the following pieces of context to answer the question at the end.
-The context contains question-answer pairs and their links from Stackoverflow.
-You should prefer information from accepted or more upvoted answers.
-Make sure to rely on information from the answers and not on questions to provide accurate responses.
-When you find particular answer in the context useful, make sure to cite it in the answer using the link.
+The context contains snippets from docs.release.com the Release documentation.
+When you find particular documentation snippet useful, make sure to cite it in the answer using the link.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
 ----
 {summaries}
 ----
 Each answer you generate should contain a section at the end of links to
-Stackoverflow questions and answers you found useful, which are described under Source value.
-You can only use links to StackOverflow questions that are present in the context and always
+You can only use links to docs.release.com that are present in the context and always
 add links to the end of the answer in the style of citations.
 Generate concise answers with references sources section of links to
-relevant StackOverflow questions only at the end of the answer.
+relevant docs.release.com documentation only at the end of the answer.
 """
 general_user_template = "Question:```{question}```"
 messages = [
